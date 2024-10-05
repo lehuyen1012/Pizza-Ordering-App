54d16e21b04d447eedf0e7008985b8fd34f0bd16
lehuyen1012 <thanhhuyen101203@gmail.com>
2024-10-05 15:33:37 +0700

    11.  Additional Shopping Cart Features

--- src/providers/CartProvider.tsx
+++ src/providers/CartProvider.tsx
@@ -5,12 +5,14 @@ type CartType = {
     items: CartItem[];
     addItem: (product: Product, size: CartItem["size"]) => void;
     updateQuantity: (itemId: string, amount: -1 | 1) => void;
+    total: number;
 };
 
 const CartContext = createContext<CartType>({
     items: [],
     addItem: () => {},
     updateQuantity: () => {},
+    total: 0,
 });
 
 const CartProvider = ({ children }: PropsWithChildren) => {
@@ -51,8 +53,12 @@ const CartProvider = ({ children }: PropsWithChildren) => {
         );
     };
 
+    const total = items.reduce(
+        (sum, item) => (sum += item.product.price * item.quantity),
+        0
+    );
     return (
-        <CartContext.Provider value={{ items, addItem, updateQuantity }}>
+        <CartContext.Provider value={{ items, addItem, updateQuantity, total }}>
             {children}
         </CartContext.Provider>
     );
