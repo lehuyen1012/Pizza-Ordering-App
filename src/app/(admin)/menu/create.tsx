7ecd79f0126276eecf4cf2033b5bae260d50b799
lehuyen1012 <thanhhuyen101203@gmail.com>
2024-10-05 16:48:29 +0700

    14. Image Picker Implementation

--- src/app/(admin)/menu/create.tsx
+++ src/app/(admin)/menu/create.tsx
@@ -3,11 +3,13 @@ import React, { useState } from "react";
 import Button from "@/components/Button";
 import { defaultPizzaImage } from "@/components/ProductListItem";
 import Colors from "@/constants/Colors";
+import * as ImagePicker from "expo-image-picker";
 
 const CreateProductScreen = () => {
     const [name, setName] = useState("");
     const [price, setPrice] = useState("");
     const [errors, setErrors] = useState("");
+    const [image, setImage] = useState<string | null>(null);
     const resetFields = () => {
         setName("");
         setPrice("");
@@ -37,10 +39,29 @@ const CreateProductScreen = () => {
         resetFields();
     };
 
+    const pickImage = async () => {
+        // No permissions request is necessary for launching the image library
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            allowsEditing: true,
+            aspect: [4, 3],
+            quality: 1,
+        });
+
+        if (!result.canceled) {
+            setImage(result.assets[0].uri);
+        }
+    };
+
     return (
         <View style={styles.container}>
-            <Image source={{ uri: defaultPizzaImage }} style={styles.image} />
-            <Text style={styles.textButton}>Select Image</Text>
+            <Image
+                source={{ uri: image || defaultPizzaImage }}
+                style={styles.image}
+            />
+            <Text onPress={pickImage} style={styles.textButton}>
+                Select Image
+            </Text>
             <Text style={styles.label}>Name</Text>
             <TextInput
                 value={name}
