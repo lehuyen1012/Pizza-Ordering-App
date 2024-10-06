fcdc5102b65adabaa6ef47e4516a024eb14987ac
lehuyen1012 <thanhhuyen101203@gmail.com>
2024-10-06 11:56:51 +0700

    16. Authentication Screen for the Sign in and Sign up

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -67,6 +67,10 @@ function RootLayoutNav() {
                         options={{ headerShown: false }}
                     />
                     <Stack.Screen
+                        name="(auth)"
+                        options={{ headerShown: false }}
+                    />
+                    <Stack.Screen
                         name="cart"
                         options={{ presentation: "modal" }}
                     />
