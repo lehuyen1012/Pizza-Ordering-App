14c08642aa4ef598950c54514178421f244eb253
lehuyen1012 <thanhhuyen101203@gmail.com>
2024-10-09 13:41:56 +0700

    17. Storage Implementation with Supabase

--- src/app/(admin)/menu/create.tsx
+++ src/app/(admin)/menu/create.tsx
@@ -12,6 +12,11 @@ import {
     useProduct,
     useUpdateProduct,
 } from "@/api/products";
+import * as FileSystem from "expo-file-system";
+import { randomUUID } from "expo-crypto";
+import { supabase } from "@/lib/supabase";
+import { decode } from "base64-arraybuffer";
+
 const CreateProductScreen = () => {
     const [name, setName] = useState("");
     const [price, setPrice] = useState("");
@@ -68,12 +73,14 @@ const CreateProductScreen = () => {
             onCreate();
         }
     };
-    const onCreate = () => {
+    const onCreate = async () => {
         if (!validateInput()) {
             return;
         }
+        const imagePath = await uploadImage();
+
         insertProduct(
-            { name, price: parseFloat(price), image },
+            { name, price: parseFloat(price), image: imagePath },
             {
                 onSuccess: () => {
                     resetFields();
@@ -117,6 +124,27 @@ const CreateProductScreen = () => {
                 router.replace("/(admin)");
             },
         });
+    };
+
+    const uploadImage = async () => {
+        if (!image?.startsWith("file://")) {
+            return;
+        }
+
+        const base64 = await FileSystem.readAsStringAsync(image, {
+            encoding: "base64",
+        });
+        const filePath = `${randomUUID()}.png`;
+        const contentType = "image/png";
+        const { data, error } = await supabase.storage
+            .from("product-images")
+            .upload(filePath, decode(base64), { contentType });
+
+        console.log(error);
+
+        if (data) {
+            return data.path;
+        }
     };
 
     const confirmDelete = () => {
