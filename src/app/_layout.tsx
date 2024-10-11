d2eaa497d34edbab1cd2e90e7c6d5dbefb36939a
lehuyen1012 <thanhhuyen101203@gmail.com>
2024-10-11 22:50:00 +0700

    8. Setting Expo Push Notifications

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -11,6 +11,7 @@ import { useColorScheme } from "react-native";
 import CartProvider from "@/providers/CartProvider";
 import AuthProvider from "@/providers/AuthProvider";
 import QueryProvider from "@/providers/QueryProvider";
+import NotificationProvider from "@/providers/NotificationProvider";
 export { ErrorBoundary } from "expo-router";
 import { StripeProvider } from "@stripe/stripe-react-native";
 
@@ -58,26 +59,28 @@ function RootLayoutNav() {
             >
                 <AuthProvider>
                     <QueryProvider>
-                        <CartProvider>
-                            <Stack>
-                                <Stack.Screen
-                                    name="(admin)"
-                                    options={{ headerShown: false }}
-                                />
-                                <Stack.Screen
-                                    name="(user)"
-                                    options={{ headerShown: false }}
-                                />
-                                <Stack.Screen
-                                    name="(auth)"
-                                    options={{ headerShown: false }}
-                                />
-                                <Stack.Screen
-                                    name="cart"
-                                    options={{ presentation: "modal" }}
-                                />
-                            </Stack>
-                        </CartProvider>
+                        <NotificationProvider>
+                            <CartProvider>
+                                <Stack>
+                                    <Stack.Screen
+                                        name="(admin)"
+                                        options={{ headerShown: false }}
+                                    />
+                                    <Stack.Screen
+                                        name="(user)"
+                                        options={{ headerShown: false }}
+                                    />
+                                    <Stack.Screen
+                                        name="(auth)"
+                                        options={{ headerShown: false }}
+                                    />
+                                    <Stack.Screen
+                                        name="cart"
+                                        options={{ presentation: "modal" }}
+                                    />
+                                </Stack>
+                            </CartProvider>
+                        </NotificationProvider>
                     </QueryProvider>
                 </AuthProvider>
             </StripeProvider>
